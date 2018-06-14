20eaf6d596ebb4bdf2078bfd47f78743569bbd27
rajesh <mirjarajesh@gmail.com>
2018-06-14 17:46:08 +0530

    commit2

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void Appium(){
+		System.out.println("AppiumTest pass");
+	}
+	@Test
+	public void Appium2(){
+		System.out.println("AppiumTest2 pass");
+	}
+
+}
---
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void selenium(){
+		System.out.println("SeleniumTest pass");
+	}
+	@Test
+	public void selenium2(){
+		System.out.println("SeleniumTest2 pass");
+	}
+
+}
